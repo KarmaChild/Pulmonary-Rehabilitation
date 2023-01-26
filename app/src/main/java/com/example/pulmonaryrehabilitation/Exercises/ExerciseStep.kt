815673c7b03e0555bb042b28272c4a03d532f2db
Ian Kohlert <ibk456@usask.ca>
2023-01-26 12:46:40 -0600

    Test Commit

--- app/src/main/java/com/example/pulmonaryrehabilitation/Exercises/ExerciseStep.kt
+++ app/src/main/java/com/example/pulmonaryrehabilitation/Exercises/ExerciseStep.kt
@@ -7,7 +7,9 @@ interface ExerciseStep {
     val stepTitle: String
     val instruction: String
     val stepType: StepType
-    val visual: View // View type might be the wrong type but this will be a view with the icon or timer or whatever
-    val videoName: VideoNames // we'll have a video loading class that loads the clip based of the name
+    val visual: View // View type might be the wrong type but this will be
+    // a view with the icon or timer or whatever
+    val videoName: VideoNames // we'll have a video loading class that loads the clip
+    // based of the name
     fun action() {}
 }
