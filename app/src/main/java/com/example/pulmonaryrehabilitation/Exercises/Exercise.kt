e05c7740d0694dbed16b879c09ee9e8348c3e212
Ian Kohlert <iankohlert@gmail.com>
2023-01-26 13:14:01 -0600

    Fixed comment line length

--- app/src/main/java/com/example/pulmonaryrehabilitation/Exercises/Exercise.kt
+++ app/src/main/java/com/example/pulmonaryrehabilitation/Exercises/Exercise.kt
@@ -4,7 +4,8 @@ interface Exercise {
     val exerciseName: String
     val exerciseSteps: List<ExerciseStep>
     val completionPoints: Int // for gamification
-    // We create 2 stacks of steps which allows a user to go back to the previous if they want
+    // We create 2 stacks of steps which allows a user to go
+    // back to the previous if they want
     var doStack: ArrayDeque<ExerciseStep>
     var undoStack: ArrayDeque<ExerciseStep>
     fun addAllStepsToDoStack()
--- app/src/main/java/com/example/pulmonaryrehabilitation/Exercises/StepType.kt
+++ app/src/main/java/com/example/pulmonaryrehabilitation/Exercises/StepType.kt
@@ -1,5 +1,7 @@
 package com.example.pulmonaryrehabilitation.Exercises
 
-enum class StepType { // this is used to give the exercise player a way to know what the step does and what actions it can do
+enum class StepType {
+    // this is used to give the exercise player a way to know what the step
+    // does and what actions it can do
     GATHER, TIMER, INTERACTIVE
 }
