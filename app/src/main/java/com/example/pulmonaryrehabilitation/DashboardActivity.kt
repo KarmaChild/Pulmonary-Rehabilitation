6b8999771b9b6f5fe8eb07d9ac7a8f45949d950a
youngH <vin734@usask.ca>
2023-01-27 05:09:46 -0600

    Fix UI

--- app/src/main/java/com/example/pulmonaryrehabilitation/DashboardActivity.kt
+++ app/src/main/java/com/example/pulmonaryrehabilitation/DashboardActivity.kt
@@ -1,24 +1,16 @@
 package com.example.pulmonaryrehabilitation
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // hide status bar
-        if (Build.VERSION.SDK_INT < 16) {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
+        // hide action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar()?.hide()
         }
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        actionBar?.hide()
 
         setContentView(R.layout.activity_dashboard)
     }
