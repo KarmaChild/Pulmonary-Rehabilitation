a51291d12d277240d08f84f0e4945d1a3c44d320
Ian Kohlert <iankohlert@gmail.com>
2023-02-20 13:19:16 -0600

    TimerExercisePlayer: Timer action added

--- app/src/main/java/com/example/pulmonaryrehabilitation/ExercisePlayerTimerViewActivity.kt
+++ app/src/main/java/com/example/pulmonaryrehabilitation/ExercisePlayerTimerViewActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pulmonaryrehabilitation
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,7 @@ import com.example.pulmonaryrehabilitation.exerciseplayer.ExercisePlayers
 
 class ExercisePlayerTimerViewActivity : AppCompatActivity() {
     lateinit var exercisePlayer: ExercisePlayer // this will be passed in
+    lateinit var timer: CountDownTimer
 
     lateinit var stepTitle: TextView
     lateinit var stepDescription: TextView
@@ -23,12 +25,42 @@ class ExercisePlayerTimerViewActivity : AppCompatActivity() {
         exercisePlayer.goToNextStep()
         // END Testing
 
+        // Initialize views
         stepTitle = findViewById<TextView>(R.id.stepTitleLabel)
         stepDescription = findViewById<TextView>(R.id.stepDescriptionLabel)
         progressBar = findViewById<ProgressBar>(R.id.timerProgressBar)
+
+        startStep()
+    }
+    fun startStep() {
         updateLabelsWithData()
         updateProgressBarWithData()
+        val step = getCurrentStep()
+        if (step?.javaClass?.kotlin == TimerStep::class) {
+            val timerStep = step as TimerStep
+            startTimer(timerStep.duration)
+        }
     }
+    fun endStep() {
+    }
+    // timer for timer steps
+    // goes to next step automatically
+    // updates UI
+    fun startTimer(time: Int) {
+        var timeLeft = time
+        var timeElapsed = 0
+        timer = object : CountDownTimer(time.toLong() * 1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+
+                timeElapsed += 1
+                progressBar.progress = timeElapsed
+            }
+            override fun onFinish() {
+                endStep()
+            }
+        }.start()
+    }
+
     fun updateLabelsWithData() {
         val step = getCurrentStep()
         stepTitle.text = step?.stepTitle
@@ -46,3 +78,19 @@ class ExercisePlayerTimerViewActivity : AppCompatActivity() {
         return exercisePlayer.exerciseRoutine.getCurrentExercise()?.getCurrentStep()
     }
 }
+// goes to the previous step
+// known bug when going from previous exercise, it has an error on tap one but then goes
+//fun previousTapped() {
+//    exercisePlayer.goToPreviousStep()
+//    if (this::timer.isInitialized) {
+//        timer.cancel()
+//    }
+//    startCurrentStep()
+//}
+//fun nextTapped() {
+//    exercisePlayer.goToNextStep()
+//    if (this::timer.isInitialized) {
+//        timer.cancel()
+//    }
+//    startCurrentStep()
+//}
