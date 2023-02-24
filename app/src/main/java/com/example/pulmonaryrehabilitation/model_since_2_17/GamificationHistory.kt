9b66323228eb9f9d4223210f8f6ccc9e47ad0fc6
adith <adm115@mail.usask.ca>
2023-02-23 20:20:16 -0600

    resolve merge conflict

--- app/src/main/java/com/example/pulmonaryrehabilitation/model_since_2_17/GamificationHistory.kt
+++ app/src/main/java/com/example/pulmonaryrehabilitation/model_since_2_17/GamificationHistory.kt
@@ -1,8 +1,8 @@
 package com.example.pulmonaryrehabilitation.model_since_2_17
 
 interface GamificationHistory {
-    var event: String
-    var points: String
+    var itemname: String
+    var itemname2: String
 
     override fun toString(): String
 //    fun toGamificationHistoryMap(): Map<String, Any>
--- app/src/main/java/com/example/pulmonaryrehabilitation/model_since_2_17/GamificationHistoryClass.kt
+++ app/src/main/java/com/example/pulmonaryrehabilitation/model_since_2_17/GamificationHistoryClass.kt
@@ -1,20 +1,39 @@
 package com.example.pulmonaryrehabilitation.model_since_2_17
 
-import com.google.firebase.database.Exclude
-
 class GamificationHistoryClass(
-    override var event: String,
-    override var points: String
-) : GamificationHistory {
-    override fun toString(): String {
-        return "{event='$event', points='$points'}"
+    override var itemname: String,
+    override var itemname2: String
+) : GamificationHistory, Comparable<GamificationHistoryClass> {
+    override fun compareTo(other: GamificationHistoryClass): Int {
+        println("Hello")
+        val other1 = other.itemname
+        val other2 = other.itemname2
+        if (itemname != other1) {
+            return -1
+        }
+        if (itemname2 != other2) {
+            return -1
+        }
+        return 0
     }
 
-    @Exclude
-    fun toGamificationHistoryClassMap(): Map<String, Any> {
-        return mapOf(
-            "event" to event,
-            "points" to points
-        )
+    override fun toString(): String {
+        return "{itemname='$itemname', itemname2='$itemname2'}"
     }
 }
+
+/*
+Rectangle(val length: Int, val breadth: Int): Comparable<Rectangle>{
+    override fun compareTo(other: Rectangle): Int {
+        val area1 = length * breadth
+        val area2 = other.length * other.breadth
+
+        // Comparing two rectangles on the basis of area
+        if(area1 == area2){
+            return 0;
+        }else if(area1 < area2){
+            return -1;
+        }
+        return 1;
+    }
+ */
