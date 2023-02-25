0c96628c20c471cdca5102a9156ef2c881239882
Ian Kohlert <iankohlert@gmail.com>
2023-02-25 16:25:06 -0600

    Video won't play bug. Seems to be an issue with vertical .mp4 files. I uploaded a .MOV file and the video played (although was very choppy on my emulator)

--- app/src/main/java/com/example/pulmonaryrehabilitation/VideoNames.kt
+++ app/src/main/java/com/example/pulmonaryrehabilitation/VideoNames.kt
@@ -2,5 +2,5 @@ package com.example.pulmonaryrehabilitation
 
 enum class VideoNames(val resource: Int) {
     // why is the paramater Int? I guess that's how Android identifies resource items
-    TEST(R.raw.testvideo)
+    TEST(R.raw.testvideo2)
 }
