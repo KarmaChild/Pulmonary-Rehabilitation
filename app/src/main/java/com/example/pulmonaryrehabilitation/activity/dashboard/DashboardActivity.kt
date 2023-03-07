b41b6940b410b0728369c21e6610eaf03685ea71
Ian Kohlert <iankohlert@gmail.com>
2023-03-07 09:54:02 -0600

    Implemented: All dashboard widget views Connected the views to the Activity

--- app/src/main/java/com/example/pulmonaryrehabilitation/activity/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/example/pulmonaryrehabilitation/activity/dashboard/DashboardActivity.kt
@@ -2,12 +2,24 @@ package com.example.pulmonaryrehabilitation.activity.dashboard
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import com.example.pulmonaryrehabilitation.R
 import com.example.pulmonaryrehabilitation.member.CurrentUser
 
 class DashboardActivity : AppCompatActivity() {
+    lateinit var exerciseCollection: ImageView
+    lateinit var streakTextView: TextView
+    lateinit var stepCountTextView: TextView
+    lateinit var stepGoalTextView: TextView
+    lateinit var diseaseTextView: TextView
+    lateinit var collectionNameTextView: TextView
+    lateinit var collectionDescriptionTextView: TextView
+    lateinit var collectionTimeTextView: TextView
+    lateinit var collectionView: ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // hide action bar
@@ -17,10 +29,27 @@ class DashboardActivity : AppCompatActivity() {
         val currentUserId = CurrentUser.getUserId()
         Log.d("DashBoardActivity", currentUserId)
 
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_dashboard2)
+
+        exerciseCollection = findViewById(R.id.greenBackground)
+        exerciseCollection.setOnClickListener {
+            goToExerciseCollection()
+        }
+        streakTextView = findViewById(R.id.dashboardStreakTextView)
+        stepCountTextView = findViewById(R.id.dashboardStepsTextView)
+        stepGoalTextView = findViewById(R.id.dashboardStepsGoalTextView)
+        diseaseTextView = findViewById(R.id.dashboardDiseaseTextView)
+        collectionNameTextView = findViewById(R.id.dashboardCollectionNameTextView)
+        collectionDescriptionTextView = findViewById(R.id.dashboardCollectionDescription)
+        collectionTimeTextView = findViewById(R.id.dashboardCollectionTime)
+
+        collectionView = findViewById(R.id.dashboardCollection)
+
         // show users steps and streaks
         initializeStepCounter()
         initializeStreaks()
+        initializeCollection()
+//        setCollectionVisibility(false)
     }
 
     /**
@@ -28,9 +57,10 @@ class DashboardActivity : AppCompatActivity() {
      * Later On it will display the step Counter
      */
     fun initializeStepCounter() {
-        val stepDisplay = findViewById<TextView>(R.id.stepDisplayText)
+//        val stepDisplay = findViewById<TextView>(R.id.stepDisplayText)
+        stepCountTextView.text = "1000"
         val userStepGoals: String = CurrentUser.getStepGoal().toString()
-        stepDisplay.text = userStepGoals
+        stepGoalTextView.text = userStepGoals
     }
 
     /**
@@ -38,8 +68,30 @@ class DashboardActivity : AppCompatActivity() {
      *  Later on when implemented it will display the users streaks
      */
     fun initializeStreaks() {
-        val streakDisplay = findViewById<TextView>(R.id.streakNumber)
         val userStreaks: String = CurrentUser.getStepGoal().toString()
-        streakDisplay.text = userStreaks
+        streakTextView.text = userStreaks
+    }
+
+    /*
+    Grab the collection information and populate the fields
+     */
+    fun initializeCollection() {
+        diseaseTextView.text = "COPD"
+        collectionNameTextView.text = "Week 2: Day 1"
+        collectionDescriptionTextView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue fermentum libero, et accumsan nisi laoreet in. Vestibulum tempor, quam vel ultrices ornare."
+        collectionTimeTextView.text = "37m"
+    }
+
+    /*
+    If there is no collection to do (for example they have already done one today) you can hide this
+     */
+    fun setCollectionVisibility(isVisible: Boolean) {
+        collectionView.isVisible = isVisible
+    }
+    /*
+        This will grab the exercise collection and send it to the exercise player
+     */
+    fun goToExerciseCollection() {
+        println("Hello")
     }
 }
