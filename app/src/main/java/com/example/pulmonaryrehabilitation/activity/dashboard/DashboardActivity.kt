f7c3dea7f5e401001728fd426c6d05880e113052
henry <vin734@usask.ca>
2023-03-08 22:51:07 +0700

    update DashboardActivity

--- app/src/main/java/com/example/pulmonaryrehabilitation/activity/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/example/pulmonaryrehabilitation/activity/dashboard/DashboardActivity.kt
@@ -1,23 +1,37 @@
 package com.example.pulmonaryrehabilitation.activity.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pulmonaryrehabilitation.R
+import com.example.pulmonaryrehabilitation.activity.exerciseplayeractivity.ExercisePlayerTapViewActivity
 import com.example.pulmonaryrehabilitation.member.CurrentUser
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var toTapView: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
         // hide action bar
-        if (getSupportActionBar() != null) {
-            getSupportActionBar()?.hide()
+        if (supportActionBar != null) {
+            supportActionBar?.hide()
         }
+
+        toTapView = findViewById(R.id.toTapView)
+        toTapView.setOnClickListener {
+            val intent = Intent(this, ExercisePlayerTapViewActivity :: class.java)
+            startActivity(intent)
+            finish()
+        }
+
         val currentUserId = CurrentUser.getUserId()
         Log.d("DashBoardActivity", currentUserId)
 
-        setContentView(R.layout.activity_dashboard)
         // show users steps and streaks
         initializeStepCounter()
         initializeStreaks()
@@ -27,7 +41,7 @@ class DashboardActivity : AppCompatActivity() {
      * For now this function Just displays step Goals
      * Later On it will display the step Counter
      */
-    fun initializeStepCounter() {
+    private fun initializeStepCounter() {
         val stepDisplay = findViewById<TextView>(R.id.stepDisplayText)
         val userStepGoals: String = CurrentUser.getStepGoal().toString()
         stepDisplay.text = userStepGoals
@@ -37,7 +51,7 @@ class DashboardActivity : AppCompatActivity() {
      * For now This function displays step goals as well
      *  Later on when implemented it will display the users streaks
      */
-    fun initializeStreaks() {
+    private fun initializeStreaks() {
         val streakDisplay = findViewById<TextView>(R.id.streakNumber)
         val userStreaks: String = CurrentUser.getStepGoal().toString()
         streakDisplay.text = userStreaks
