a9c5f938440ec248e7207a0673e19bf8b2d4e1c5
Allan Bifom (aeb672) <aeb672@usask.ca>
2023-03-08 18:50:44 -0600

    commiting to checkout

---
+++ app/src/main/java/com/example/pulmonaryrehabilitation/activity/streaks/StreakActivity.kt
@@ -0,0 +1,54 @@
+package com.example.pulmonaryrehabilitation.activity.streaks
+
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.collections.ArrayList
+import com.example.pulmonaryrehabilitation.R
+
+class StreakActivity : AppCompatActivity() {
+
+    // variables for grid view and badge list
+    lateinit var badgeGridView: GridView
+    lateinit var badgeList: List<StreakBadgeData>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // initializing variables of grid view with their ids.
+        badgeGridView = findViewById(R.id.idGridView)
+        badgeList = ArrayList<StreakBadgeData>()
+
+        // on below line we are adding data to
+        // our badge list with image and badge name, as well as description and unlock Id.
+
+
+//        badgeList = badgeList + StreakBadgeData("C++", R.drawable.c)
+//        badgeList = badgeList + StreakBadgeData("Java", R.drawable.java)
+//        badgeList = badgeList + StreakBadgeData("Android", R.drawable.android)
+//        badgeList = badgeList + StreakBadgeData("Python", R.drawable.python)
+//        badgeList = badgeList + StreakBadgeData("Javascript", R.drawable.js)
+
+        // on below line I am initializing our badge adapter
+        // and passing course list and context.
+        val badgeAdapter = StreakBadgeView(badgeList = badgeList, this@StreakActivity)
+
+        // on below line we are setting adapter to our grid view.
+        badgeGridView.adapter = badgeAdapter
+
+        // on below line we are adding on item
+        // click listener for our grid view.
+        badgeGridView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            // inside on click method we are simply displaying
+            // a toast message with course name.
+            Toast.makeText(
+                applicationContext, badgeList[position].badgeName + " selected",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
